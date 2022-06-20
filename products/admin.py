2710cd8f51c4a82f1c90e155c73bcc780e75c722
VinBdev <vbpod77@gmail.com>
2022-06-20 18:38:35 +0000

    Edited prodcut fixtures

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.Register(Product)
-admin.site.Register(Category)
+admin.site.register(Product)
+admin.site.register(Category)
