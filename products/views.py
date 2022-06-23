c76bc95efe6f9469d7c272a1c5997136fc2b93bd
VinBdev <vbpod77@gmail.com>
2022-06-23 22:47:31 +0000

    added sorting/ product conut ot product.html

--- products/views.py
+++ products/views.py
@@ -3,17 +3,16 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 
-# Created views here below.
+# Created views below:
 
 def all_products(request):
-    """ A view to show all products as well as include sorting and searching """
+    """ A view to show all products as well as include """
 
     products = Product.objects.all()
     query = None
     categories = None
     sort = None
     direction = None
-    
 
     if request.GET:
         if 'sort' in request.GET:
@@ -22,14 +21,14 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
-        if 'direction' in request.GET:
-            direction = request.GET['direction']
-            if direction == 'desc':
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -38,32 +37,32 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "Searching could not verify your criteria")
+                messages.error(request, "Please enter correct search criteria!")
                 return redirect(reverse('products'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
- 
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
-    
+
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-    """ A view to show seperate product details """
+    """ View to show seperate product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
         'product': product,
     }
-    
+
     return render(request, 'products/product_detail.html', context)
     
