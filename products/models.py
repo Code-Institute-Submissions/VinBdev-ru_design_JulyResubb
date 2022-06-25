f9d04b8fe213741fa7cffa0fafcef48816b37bf5
VinBdev <vbpod77@gmail.com>
2022-06-25 14:38:11 +0000

    added sizing models to product.models

--- products/models.py
+++ products/models.py
@@ -11,10 +11,10 @@ class Category(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.name 
+        return self.name
 
     def get_friendly_name(self):
-        return self.friendly_name 
+        return self.friendly_name
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
@@ -22,6 +22,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True) 
     image = models.ImageField(max_length=1024, null=True, blank=True)
 
