86dd4b077c69a7c5f03c45e2fe43dfd5092611c1
VinBdev <vbpod77@gmail.com>
2022-06-26 22:17:29 +0000

    Completed webhook event handlers

--- checkout/views.py
+++ checkout/views.py
@@ -9,12 +9,13 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
 
 
 @require_POST
 def cache_checkout_data(request):
     try:
-        pid = request.POST.get('client_secret').split('secret')[0]
+        pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY 
         stripe.PaymentIntent.modify(pid, metadata={
             'bag': json.dumps(request.session.get('bag', {})),
@@ -22,16 +23,16 @@ def cache_checkout_data(request):
             'username': request.user,
         })
         return HttpResponse(status=200)
+
     except Exception as e:
         messages.error(request, 'Sorry your payment cannot be processed. Please try again.')
-        return HttpResponse(content=e, status=200)
-
-
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+
 
     if request.method == "POST":
         bag = request.session.get("bag", {})
@@ -50,7 +51,11 @@ def checkout(request):
 
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            request.POST.get('client_secret').split('secret')[0]
+            order.stripe_pid = pid 
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
