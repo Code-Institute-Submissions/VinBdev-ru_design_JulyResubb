73f89de070d268d3e3f43d3b41fe866cee70eb96
VinBdev <vbpod77@gmail.com>
2022-06-27 15:01:39 +0000

    started fixing image section of product edit page

--- products/forms.py
+++ products/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from .models import Product, Category
-
+from .widgets import CustomclearableFileInput
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomclearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super.().__init__(*args, **kwargs)
