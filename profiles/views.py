74bc0c2094d35b61eda4c7b90c8dccdb48667f33
VinBdev <vbpod77@gmail.com>
2022-06-27 14:37:22 +0000

    Secured the views for admin/ superuser only

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
+@login_required
 def profile(request):
     """Displaying users profile """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -28,6 +30,7 @@ def profile(request):
         'on_profile_page': True
     }
 
+
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
@@ -43,3 +46,4 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
+    
